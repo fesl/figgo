9997f56146f6df65fa6c72c76cf9af5ee0a43329
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-08 18:24:49 -0300

    renamed some methods and authorization action mapping

--- src/main/webapp/WEB-INF/groovy/bank.groovy
+++ src/main/webapp/WEB-INF/groovy/bank.groovy
@@ -1,24 +1,24 @@
 def actions = ['index', 'transfer', 'statement']
 
 if (actions.contains(params.action)) {
-	actionCall = "action_" + request.method.toLowerCase() + "_" + params.action
+	actionCall = request.method.toLowerCase() + "_" + params.action
 } else {
 	actionCall = "notfound"
 }
 
-def action_get_index() {
+def get_index() {
 	render 'bank/index.vm', request, response
 }
 
-def action_get_transfer() {
+def get_transfer() {
 	render 'bank/transfer.vm', request, response
 }
 
-def action_get_statement() {
+def get_statement() {
 	render 'bank/statement-choice.vm', request, response
 }
 
-def action_post_transfer() {
+def post_transfer() {
 	redirect 'http://mundo.figgo.com.br:8080'
 }
 
