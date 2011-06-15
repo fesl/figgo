0577fef495d5fdbd3aa8664ed3753d86314b7b4a
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-15 17:55:15 -0300

    route53 problems solved

--- src/main/java/br/octahedron/straight/modules/admin/util/Route53Util.java
+++ src/main/java/br/octahedron/straight/modules/admin/util/Route53Util.java
@@ -19,16 +19,17 @@
 package br.octahedron.straight.modules.admin.util;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.Map.Entry;
+import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -52,11 +53,13 @@ public class Route53Util {
 
 	private static final Logger logger = Logger.getLogger(Route53Util.class.getName());
 	private static final URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
+	private static final SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US);
 
 	private static final String ROUTE53_SERVER = "https://route53.amazonaws.com";
 	private static final String DATE_COMMAND = "/date";
 	private static final String VERSION_SPEC = "/2011-05-05";
 	private static final String HOSTED_ZONE_COMMAND = "/hostedzone/";
+	private static final String RRSET = "/rrset";
 
 	private static final String ACCESS_ID_TOKEN = "{accessid}";
 	private static final String SIGNATURE_TOKEN = "{sign}";
@@ -65,14 +68,18 @@ public class Route53Util {
 	private static final String KEY_ALGORITHM = "PBKDF2WithHmacSHA1";
 	private static final String MAC_ALGORITHM = "HmacSHA1";
 
-	private static final String AUTH_TOKEN = "AWS3-HTTPS AWSAccessKeyId={accessid},Algorithm=" + MAC_ALGORITHM + ",Signature=${sign}";
+	private static final String AUTH_TOKEN = "AWS3-HTTPS AWSAccessKeyId={accessid},Algorithm=" + MAC_ALGORITHM + ",Signature={sign}";
 	private static final String FETCH_DATE_HEADER = "Date";
 	private static final String SUBMIT_DATE_HEADER = "x-amz-date";
 	private static final String AUTHORIZATION_HEADER = "X-Amzn-Authorization";
 
+	static {
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+	}
+
 	public static void createDomain(String domain, String accessId, String accessKey, String hostedZoneId) throws Route53Exception {
 		try {
-			HTTPRequest request = new HTTPRequest(new URL(ROUTE53_SERVER + VERSION_SPEC + HOSTED_ZONE_COMMAND + hostedZoneId), HTTPMethod.POST);
+			HTTPRequest request = new HTTPRequest(new URL(ROUTE53_SERVER + VERSION_SPEC + HOSTED_ZONE_COMMAND + hostedZoneId + RRSET), HTTPMethod.POST);
 			String requestBody = generateRequestBody(domain);
 			logger.fine(requestBody);
 			request.setPayload(requestBody.getBytes());
@@ -90,9 +97,8 @@ public class Route53Util {
 	}
 
 	protected static void signRequest(HTTPRequest request, String accessID, String key) throws IOException, InvalidKeyException,
-			InvalidKeySpecException, NoSuchAlgorithmException {
+	InvalidKeySpecException, NoSuchAlgorithmException {
 		String date = fetchDate();
-		logger.fine("##################**********" + date);
 		String sign = sign(date, key);
 		String signature = AUTH_TOKEN.replace(ACCESS_ID_TOKEN, accessID);
 		signature = signature.replace(SIGNATURE_TOKEN, sign);
@@ -102,19 +108,18 @@ public class Route53Util {
 		request.addHeader(new HTTPHeader("Content-Type", "text/xml; charset=UTF-8"));
 	}
 
-	protected static String sign(String content, String key) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException {
-		SecretKey skey = new SecretKeySpec(key.getBytes(), KEY_ALGORITHM);
+	protected static String sign(String content, String key) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, IllegalStateException {
 		Mac mac = Mac.getInstance(MAC_ALGORITHM);
+		SecretKey skey = new SecretKeySpec(key.getBytes(), KEY_ALGORITHM);
 		mac.init(skey);
 		mac.update(content.getBytes());
-		return new String(Base64.encodeBase64(mac.doFinal()));
+		return new String(Base64.encodeBase64((mac.doFinal())));
 	}
 
 	protected static String fetchDate() throws IOException {
 		HttpURLConnection connection = (HttpURLConnection) new URL(ROUTE53_SERVER + DATE_COMMAND).openConnection();
 		if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-			Map<String, List<String>> headers = connection.getHeaderFields();
-			return new Date(connection.getHeaderFieldDate(FETCH_DATE_HEADER,0)).toGMTString();
+			return formatter.format(new Date(connection.getHeaderFieldDate(FETCH_DATE_HEADER,0)));
 		} else {
 			return null;
 		}
