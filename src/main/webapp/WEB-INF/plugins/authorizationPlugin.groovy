0f8cd75da9450a5fe38eaebbc527611317cce70f
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-14 21:09:47 -0300

    using authorization manager from model

--- src/main/webapp/WEB-INF/plugins/authorizationPlugin.groovy
+++ src/main/webapp/WEB-INF/plugins/authorizationPlugin.groovy
@@ -1,9 +1,12 @@
 def properties = new ConfigSlurper().parse(new File('WEB-INF/plugins/authorization.properties').toURL())
 def log = new groovyx.gaelyk.logging.GroovyLogger("br.octahedron.straight.view.plugins.authorizationPlugin")
+def authorizationManager = ManagerBuilder.getAuthorizationManager()
 
 before {
 	def controller = request.requestURI[1..-8]
-	def controllerAction = request.method.toLowerCase() + "_" + request.parameterMap.action?.getAt(0)
-	def authorizationAction = properties."$controller"."$controllerAction"
-	// Enum."$authorizationAction"
+	def controllerAction = request.method.toLowerCase() + "_" request.parameterMap.module?.getAt(0) + request.parameterMap.action?.getAt(0)
+	def activity = properties."$controller"."$controllerAction"
+	if (controller != "user" && !authorizationManager.isAuthorized(request.serverName, request.user.email, activity) {
+		response.sendRedirect '/500'
+	}
 }
