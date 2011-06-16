5b9cab3a974d03fa4d94e1f4da02463ce745f26e
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-16 18:20:30 -0300

    added bank admin page

--- src/main/webapp/WEB-INF/groovy/bank.groovy
+++ src/main/webapp/WEB-INF/groovy/bank.groovy
@@ -1,6 +1,6 @@
 import br.octahedron.straight.modules.ManagerBuilder
 
-actions = ['index', 'transfer', 'statement']
+actions = ['index', 'transfer', 'statement', 'admin', 'ballast', 'share']
 configurationManager = ManagerBuilder.getConfigurationManager()
 accountManager = ManagerBuilder.getAccountManager()
 
@@ -14,6 +14,22 @@ def get_index() {
 	request.domain = configurationManager.getDomainConfiguration()
 	request.balance = accountManager.getBalance(request.user.email)
 	render 'bank/index.vm', request, response
+}
+
+def get_admin() {
+	request.domain = configurationManager.getDomainConfiguration()
+	request.balance = accountManager.getBalance(extractDomainName(request.serverName))
+	render 'bank/admin.vm', request, response
+}
+
+def post_share() {
+	accountManager.transact(extractDomainName(request.serverName), params.userId, params.amount, params.comment, params.type)
+	redirect '/bank/admin', request, response
+}
+
+def post_ballast() {
+	accountManager.insertBallast(extractDomainName(request.serverName), params.amount, params.comment)
+	redirect '/bank/admin', request, response
 }
 
 def get_transfer() {
