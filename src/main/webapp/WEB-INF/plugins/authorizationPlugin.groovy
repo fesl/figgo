576ebe067c9e0f095cabd9b0288a33d308264db2
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-24 11:53:45 -0300

    removed unused plugins on gaelyk

--- src/main/webapp/WEB-INF/plugins/authorizationPlugin.groovy
+++
@@ -1,8 +0,0 @@
-def properties = new ConfigSlurper().parse(new File('WEB-INF/plugins/authorization.properties').toURL())
-
-before {
-	def controller = request.requestURI[1..-8]
-	def controllerAction = request.method.toLowerCase() + "_" + request.parameterMap.module?.getAt(0) + request.parameterMap.action?.getAt(0)
-	def activity = properties."$controller"."$controllerAction"
-	request.activity = activity
-}
