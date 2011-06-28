44582f7c3acb999415405195a00d6f3bdc48c575
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-06-28 20:10:58 -0300

    listing domains of user and also restrincting domain info to unknown users

--- src/main/webapp/WEB-INF/groovy/index.groovy
+++ src/main/webapp/WEB-INF/groovy/index.groovy
@@ -1,11 +1,14 @@
+import br.octahedron.commons.database.NamespaceCommons
 import br.octahedron.straight.modules.ManagerBuilder
 
 usersManager = ManagerBuilder.getUserManager()
 configurationManager = ManagerBuilder.getConfigurationManager()
+authorizationManager = ManagerBuilder.getAuthorizationManager()
 
 def index() {
 	if (request.serverName == "www.figgo.com.br" || request.serverName == "localhost") {
 		if (request.user) {
+			request.domains = authorizationManager.getUserDomains(request.user.email)
 			request.user = usersManager.getUser(request.user.email)
 			render 'user/dashboard.vm', request, response		
 		} else {
@@ -13,7 +16,18 @@ def index() {
 		}
 	} else {
 		request.domain = configurationManager.getDomainConfiguration()
-		render 'domain/index.vm', request, response
+		try {
+			NamespaceCommons.changeToGlobalNamespace()
+			userExists = authorizationManager.getUserDomains(request.user?.email).contains(request.domain.domainName)
+		} finally {
+			NamespaceCommons.changeToPreviousNamespace()
+		}
+
+		if (userExists) {
+			render 'domain/index.vm', request, response
+		} else {
+			render 'domain/public_index.vm', request, response
+		}
 	}
 }
 
