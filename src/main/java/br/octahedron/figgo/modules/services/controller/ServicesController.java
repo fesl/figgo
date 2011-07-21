5ad611eba6055bd032472cf56594d5df7faa36f0
Danilo Penna Queiroz <daniloqueiroz@octahedron.com.br>
2011-07-20 22:03:43 -0300

    Fixed import

--- src/main/java/br/octahedron/figgo/modules/services/controller/ServicesController.java
+++ src/main/java/br/octahedron/figgo/modules/services/controller/ServicesController.java
@@ -22,7 +22,7 @@ import java.math.BigDecimal;
 
 import br.octahedron.cotopaxi.auth.AuthenticationRequired;
 import br.octahedron.cotopaxi.controller.Controller;
-import br.octahedron.cotopaxi.datastore.NamespaceRequired;
+import br.octahedron.cotopaxi.datastore.namespace.NamespaceRequired;
 import br.octahedron.cotopaxi.inject.Inject;
 import br.octahedron.cotopaxi.validation.Validator;
 import br.octahedron.figgo.modules.services.controller.validation.ServiceValidators;
