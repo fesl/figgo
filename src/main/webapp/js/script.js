33f80bed5c97485fe4a24547182547df6cdf033c
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-10-17 20:22:33 -0300

    removed unsed old code, removed cache-control metatag, rearranged application.config

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,38 +1,5 @@
 $(function() {
 	
-	$(".dropdown > a").click(function(e) {
-		var $this = $(this);
-		$(".dropdown ul").toggle();
-
-		if ($this.css("display") == "none") {
-			$this.removeClass("selected");
-		} else {
-			$this.addClass("selected");
-		}
-		
-		e.preventDefault();
-	});
-
-	$(".dropdown > ul li a").click(function() {
-	    $(".dropdown > ul").hide();
-	});
-	
-	$(document).bind('click', function(e) {
-	    // Lets hide the menu when the page is clicked anywhere but the menu.
-	    var $clicked = $(e.target);
-	    if (!$clicked.parents().hasClass("dropdown")) {
-	        $(".dropdown ul").hide();
-			$(".dropdown a").removeClass("selected");
-		}
-	});
-	
-	var theTable = $('#domains > table');
-	if (theTable.length) {
-		$('#domain-search').keyup(function() {
-			$.uiTableFilter( theTable, this.value, "Nome" );
-		});
-	}
-		
 	var searchUserInput = $(".autocomplete");
 	if (searchUserInput.length) {
 		var cache = {},
@@ -40,23 +7,23 @@ $(function() {
 		searhUserInput.autocomplete({
 			minLength: 2,
 			source: function( request, response ) {
-				var term = request.term;
-				if ( term in cache ) {
-					response( cache[ term ] );
-					return;
-				}
+						var term = request.term;
+						if ( term in cache ) {
+							response( cache[ term ] );
+							return;
+						}
 		
-				lastXhr = $.getJSON( "/user/search/" + term, function( data, status, xhr ) {
-					cache[ term ] = data;
-					if ( xhr === lastXhr ) {
-						response( $.map( data, function( item ) {
-							return {
-								label: item.name + " <" + item.userId + ">",
-								value: item.userId
-							}
-						}));
-					}
-				});
+						lastXhr = $.getJSON( "/user/search/" + term, function( data, status, xhr ) {
+						cache[ term ] = data;
+						if ( xhr === lastXhr ) {
+							response( $.map( data, function( item ) {
+								return {
+									label: item.name + " <" + item.userId + ">",
+									value: item.userId
+								}
+							}));
+						}
+					}); // source
 			}
 		});
 	}
