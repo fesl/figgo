927fd9e3bd6f445ac98edbcf9be5db93061a7849
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-10-17 22:06:11 -0300

    added datepicker to bank statement page

--- src/main/webapp/js/bank.js
+++ src/main/webapp/js/bank.js
@@ -45,5 +45,19 @@ $(function() {
 		});
 		e.preventDefault();
 	});
+
+	var dates = $( "#startDate, #endDate" ).datepicker({
+        defaultDate: "+1w",
+        numberOfMonths: 2,
+        onSelect: function( selectedDate ) {
+            var option = this.id == "startDate" ? "minDate" : "maxDate",
+                instance = $( this ).data( "datepicker" ),
+                date = $.datepicker.parseDate(
+                    instance.settings.dateFormat ||
+                    $.datepicker._defaults.dateFormat,
+                    selectedDate, instance.settings );
+            dates.not( this ).datepicker( "option", option, date );
+        }
+    });
 	
 });
