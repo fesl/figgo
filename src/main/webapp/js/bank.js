f070beee37a5611039a2835191e4d05f581fe05d
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-11-14 21:38:31 -0300

    fixed the autocomplete issue

--- src/main/webapp/js/bank.js
+++ src/main/webapp/js/bank.js
@@ -1,4 +1,42 @@
 $(function() {
+    var $searchUserInput = $(".autocomplete");
+    if ($searchUserInput.length) {
+        var cache = {},
+            lastXhr;
+        $searchUserInput.autocomplete({
+            minLength: 2,
+            source: function( request, response ) {
+                        var term = request.term;
+                        if ( term in cache ) {
+                            response(  
+                                    $.map( cache[term].result, function( item ) {
+                                        return {
+                                            label: item.name + " <" + item.userId + ">",
+                                            value: item.userId
+                                        };
+                                    })
+                            );
+                            return;
+                        }
+        
+                        lastXhr = $.getJSON( "/user/search/" + term, 
+                            function( data, status, xhr ) {
+                            cache[ term ] = data;
+                            if ( xhr === lastXhr ) {
+                                response( 
+                                    $.map( data.result, function( item ) {
+                                        return {
+                                            label: item.name + " <" + item.userId + ">",
+                                            value: item.userId
+                                        };
+                                    })
+                                );
+                            }}
+                        );
+                    }
+        });
+    }
+
     $("#statement-form").submit(function(e) {
         var $this = $(this),
         $table = $("table"),
