9439f2c5b324c4beb0a3fbe2891e9433d636cc9c
Danilo Queiroz <dpenna.queiroz@gmail.com>
2012-01-27 22:49:25 -0300

    Revert "deploy script++ / version_number convention defined"
    This reverts commit 5fe70410686baa93fa525c08b5f3e48f0e1d6f55.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,8 @@
 #! /bin/bash
 
-cat << EOF
-Olá, este script irá auxiliá-lo a realizar um deploy do figgo.
-Antes de mais nada é interessante que você certifique-se este branch contém
-as mudanças que você deseja fazer o deploy.
-Talvez você precise fazer um 'git rebase master' ou pegar apenas os patches
-que você deseja. Assegure-se disso antes de continuar.
-
-[Enter]
-EOF
-read 
-
-cat <<EOF
-O primeiro passo antes do deploy é ajustar o numero de versão no arquivo de 
-configuração do Appengine. Se você tem alguma dúvida de como atribuir o número
-da versão, olhe o arquivo 'VERSION_NUMBER'
-
-[Enter]
-EOF
-read 
-
+echo "Please, before continue, edit the application version"
+echo "Press ENTER to continue"
+read a
 vim src/main/webapp/WEB-INF/appengine-web.xml && \
 echo "Building and deploying application" && \
 mvn clean test war:exploded && appcfg.sh update target/deploy
