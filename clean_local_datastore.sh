6e07a365214f2db54d06303ec9aece993743bd9d
Danilo Queiroz <dpenna.queiroz@gmail.com>
2012-01-27 22:51:31 -0300

    Revert "Deploy script"
    This reverts commit 501a0cee5dc6d227595a7c892d17f4cda2b81368.

---
+++ clean_local_datastore.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+rm target/figgo-1.0-SNAPSHOT/WEB-INF/appengine-generated/local_db.bin
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Please, before continue, edit the application version"
-echo "Press ENTER to continue"
-read a
-vim src/main/webapp/WEB-INF/appengine-web.xml && \
-echo "Building and deploying application" && \
-mvn clean test war:exploded && appcfg.sh update target/deploy
