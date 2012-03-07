e07fbd132b8f05b8c8e244f07b0358c2c0b21cad
Danilo Queiroz <dpenna.queiroz@gmail.com>
2012-03-07 17:59:20 -0300

    deploy script changed - support to dev env added

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 version=maintenance
+appid=dev-figgo
 
 alert() {
 	cat << EOF
@@ -11,6 +12,16 @@ Este script irá auxiliá-lo nesta tarefa!
 [Enter para continuar]
 EOF
 	read 
+
+if [ ${appid} == "figgo-octa" ]
+then
+	cat << EOF
+Você está prestes a fazer um deploy no ambiente de produção.
+
+Para cancelar pressione CTRL+C, para continuar Enter.
+EOF
+	read
+fi
 	return 0
 }
 build_project() {
@@ -19,18 +30,23 @@ build_project() {
 	return $?
 }
 deploy_app() {
-	appcfg.sh -V $version update target/deploy
+	appcfg.sh -A ${appid} -V ${version} update target/deploy
 	return $?
 }
 change_default() {
 	read -p "Deseja alterar a versão default do Figgo para a nova versão? [s/n] " resp
 	if [ $resp = "s" ]; then
-		appcfg.sh -V $version set_default_version target/deploy
+		appcfg.sh -A ${appid} -V ${version} set_default_version target/deploy
 		return $?
 	else
 		return 0
 	fi
 }
+
+if [ $# -eq 2 ] && [ $1 == "--app" ]
+then
+	appid=$2
+fi
 
 alert && \
 build_project && \
