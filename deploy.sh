fe168af37acad8c51fe96b24b36b531d3b1033ba
Danilo Queiroz <dpenna.queiroz@gmail.com>
2012-03-07 18:46:41 -0300

    deploy script adapt to alfa test env

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+appid=dev-figgo
+
 alert() {
 	cat << EOF
 Olá, você está preste a realizar um novo deploy do figgo.
@@ -9,13 +11,27 @@ Este script irá auxiliá-lo nesta tarefa!
 [Enter para continuar]
 EOF
 	read 
+
+if [ ${appid} == "figgo-octa" ]
+then
+	cat << EOF
+Você está prestes a fazer um deploy no ambiente de produção.
+
+Para cancelar pressione CTRL+C, para continuar Enter.
+EOF
+	read
+fi
+
 	return 0
 }
 update_version() {
 	actual=$(cat version)
 	echo "Versão atual $actual"
-	read -p "Digite a nova versão: " version
-	echo ${version} > version
+	read -p "Digite a nova versão (ou enter para mesma versao): " version
+	if [ ${version} ]
+	then
+		echo ${version} > version
+	fi
 	return $?
 }
 build_project() {
@@ -26,23 +42,33 @@ build_project() {
 adjust_config() {
 	sed -i "/^    APPLICATION/d" target/deploy/WEB-INF/application.config && \
 	sed -i "s/^    #APPLICATION/    APPLICATION/g" target/deploy/WEB-INF/application.config 
+
+	if [ $? -eq 0 ] && [ ${appid} == "dev-figgo" ]
+	then
+		sed -i "s/figgo.com.br/oggif.com.br/g" target/deploy/WEB-INF/application.config 
+	fi
 	return $?
 }
 deploy_app() {
 	version=$(cat version)
-	appcfg.sh -V $version update target/deploy
+	appcfg.sh -A ${appid} -V ${version} update target/deploy
 	return $?
 }
 change_default() {
 	read -p "Deseja alterar a versão default do Figgo para a nova versão? [s/n] " resp
 	if [ $resp = "s" ]; then
 		version=$(cat version)
-		appcfg.sh -V $version set_default_version target/deploy
+		appcfg.sh -A ${appid} -V ${version} set_default_version target/deploy
 		return $?
 	else
 		return 0
 	fi
 }
+
+if [ $# -eq 2 ] && [ $1 == "--app" ]
+then
+	appid=$2
+fi
 
 alert && \
 update_version && \
